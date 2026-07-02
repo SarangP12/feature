c7f6664d31a6952e1ee267c3d2f3c15836192cdb
Karuna Patel <karuna.bluedigital@gmail.com>
2026-07-02 17:42:43 +0530

    changes

--- src/test/java/com/electra/automation/testcases/authentication/LoginTest.java
+++ src/test/java/com/electra/automation/testcases/authentication/LoginTest.java
@@ -1,10 +1,11 @@
 package com.electra.automation.testcases.authentication;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import com.electra.automation.base.BaseClass;
 import com.electra.automation.pages.authentication.LoginPage;
 import com.electra.automation.utilities.ConfigReader;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 public class LoginTest extends BaseClass {
     @Test(description = "Validates login page loads and login form is visible")
@@ -12,7 +13,7 @@ public class LoginTest extends BaseClass {
         LoginPage loginPage = new LoginPage(getDriver());
         Assert.assertTrue(loginPage.isLoginFormVisible(), "Login form should be visible");
         loginPage.enterUsername(ConfigReader.getValue("qa.username"));
-        loginPage.enterPassword(ConfigReader.getValue("qa.password"));
+        loginPage.enterPassword(ConfigReader.getValue("qa.password")); 
         loginPage.clickLogin();
         Thread.sleep(2000); // Wait for login to process
     }
